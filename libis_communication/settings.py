946a6364a30e3f81a308dbf6c126bebd9470dd7c
Omer fils Elenga <omerlinks@MacBook-Air-de-Omer.local>
2025-05-18 17:02:17 +0100

    ch:mise ajour

--- libis_communication/settings.py
+++ libis_communication/settings.py
@@ -210,7 +210,7 @@ if not DEBUG:
 # Email configuration
 # Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
