e8da472fda25e257a1f4a94c06b5ad1e9051667b
Omer fils Elenga <omerlinks@MacBook-Air-de-Omer.local>
2025-05-18 01:40:05 +0100

    ch:finich

--- libis_communication/settings.py
+++ libis_communication/settings.py
@@ -218,7 +218,7 @@ if not DEBUG:
 # Email Configuration (Sécurisé)
 if DEBUG == False:
 
-    EMAIL_BACKEND=config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')  
+    EMAIL_BACKEND=config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')  
     EMAIL_HOST = config('EMAIL_HOST', default='')
     EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
     EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
@@ -226,16 +226,6 @@ if DEBUG == False:
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@libis.com')
     DOMAIN = config('DOMAIN', default='localhost:8000')
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-    EMAIL_PORT = config('EMAIL_PORT', default=1025, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@libis.com')
-    DOMAIN = config('DOMAIN', default='localhost:8000')
-
 
 # Custom User Model
 AUTH_USER_MODEL = 'core.User'
