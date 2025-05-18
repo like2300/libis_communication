56b2f3256e05bcf9f8ac77f8be418d68752f29b5
Omer fils Elenga <omerlinks@MacBook-Air-de-Omer.local>
2025-05-18 02:13:00 +0100

    ch:finich2 correctif mail

--- libis_communication/settings.py
+++ libis_communication/settings.py
@@ -216,10 +216,10 @@ if not DEBUG:
 # Email configuration
 
  
-EMAIL_BACKEND=config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')  
-EMAIL_HOST = config('EMAIL_HOST', default='')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@libis.com')
